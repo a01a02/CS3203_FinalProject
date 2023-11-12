b0662b2805c595837e99707f4de6d25428cfba7f
Joshua Schmitz <josh.schmitz.dev@gmail.com>
2023-11-12 09:37:10 -0600

    firebase setup

--- twatter/src/routes/firebase.ts
+++ twatter/src/routes/firebase.ts
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+//Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAx9wSfYPpSENNp3AyeUZ-NeW3BaJIlv-w",
   authDomain: "cs3203final.firebaseapp.com",
@@ -13,5 +11,6 @@ const firebaseConfig = {
   appId: "1:47200009677:web:685ae8eb367766f60cd884"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
