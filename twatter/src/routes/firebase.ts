4ca211630027931cb36e105f0df303dbe32043d4
Joshua Schmitz <josh.schmitz.dev@gmail.com>
2023-11-12 09:35:41 -0600

    initialize firebase

---
+++ twatter/src/routes/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAx9wSfYPpSENNp3AyeUZ-NeW3BaJIlv-w",
+  authDomain: "cs3203final.firebaseapp.com",
+  projectId: "cs3203final",
+  storageBucket: "cs3203final.appspot.com",
+  messagingSenderId: "47200009677",
+  appId: "1:47200009677:web:685ae8eb367766f60cd884"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
