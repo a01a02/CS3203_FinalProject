656390e6b5fcb36bfcaa71d99d06575c9618fa06
Joshua Schmitz <josh.schmitz.dev@gmail.com>
2023-11-12 10:31:20 -0600

    4.0-1-2 firestore and tweets

--- twatter/src/routes/firebase.ts
+++ twatter/src/routes/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 //Firebase configuration
 const firebaseConfig = {
@@ -15,3 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
